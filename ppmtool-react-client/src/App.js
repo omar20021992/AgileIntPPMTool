069269bdca0e2e7042e4b09e4db9dd97756a0242
omar20021992 <omar20021992@gmail.com>
2022-11-12 14:43:14 -0800

    first commit

---
+++ ppmtool-react-client/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Header from './components/Layout/Header';
+import "bootstrap/dist/css/bootstrap.min.css"
+import Dashboard from './components/Dashboard';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from './components/Project/AddProject';
+import { Provider } from "react-redux"; 
+import store from './store';
+
+function App() {
+  return (
+    <Provider store={store}>
+     <Router>
+      <div className="App">
+        <Header />
+        <Route exact path="/Dashboard" component = {Dashboard } />
+        <Route exact path="/addProject" component = {AddProject } />
+       </div>
+     </Router>
+    </Provider>
+  );
+}
+
+export default App;
