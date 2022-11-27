3a0246ea1dbb020f63567d043ed0c79e0699a54e
omar20021992 <omar20021992@gmail.com>
2022-11-26 23:44:51 -0800

    displayed errors of PT summary, added PT to state

--- ppmtool-react-client/src/components/ProjectBoard/ProjectTasks/AddProjectTask.js
+++ ppmtool-react-client/src/components/ProjectBoard/ProjectTasks/AddProjectTask.js
@@ -23,9 +23,15 @@ class AddProjectTask extends Component {
     this.onSubmit = this.onSubmit.bind(this);
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+
   // on change
   onChange(e) {
-    this.setState({ [e.target.name]: e.target.value });
+    this.setState({[e.target.name]: e.target.value });
   }
   //on submit
   onSubmit(e) {
@@ -38,6 +44,7 @@ class AddProjectTask extends Component {
       priority: this.state.priority,
       dueDate: this.state.dueDate
     };
+    console.log(newTask );
     this.props.addProjectTask(
       this.state.projectIdentifier,
       newTask,
@@ -47,6 +54,7 @@ class AddProjectTask extends Component {
 
   render() {
     const { id } = this.props.match.params;
+    const { errors } = this.state;
 
     return (
       <div className="add-PBI">
@@ -62,12 +70,17 @@ class AddProjectTask extends Component {
                 <div className="form-group">
                   <input
                     type="text"
-                    className="form-control form-control-lg"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.summary
+                    })}
                     name="summary"
                     placeholder="Project Task summary"
                     value={this.state.summary}
                     onChange={this.onChange}
                   />
+                  {errors.summary && (
+                    <div className="invalid-feedback">{errors.summary}</div>
+                  )}
                 </div>
                 <div className="form-group">
                   <textarea
@@ -130,10 +143,16 @@ class AddProjectTask extends Component {
 }
 
 AddProjectTask.propTypes = {
-  addProjectTask: PropTypes.func.isRequired
+  addProjectTask: PropTypes.func.isRequired,
+  errors: PropTypes.object.isRequired
 };
 
+const mapStateToProps = state => ({
+  errors: state.errors
+});
+
+
 export default connect(
-  null,
+  mapStateToProps, 
   { addProjectTask }
 )(AddProjectTask);
