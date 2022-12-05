b2b99e1e363cea20d0aa0c6a13c3c9e510196a75
omar20021992 <omar20021992@gmail.com>
2022-12-04 22:07:33 -0800

    mbmb

--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/web/UserController.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/web/UserController.java
@@ -7,6 +7,7 @@ import io.agileintelligence.ppmtool.validator.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+
+import static io.agileintelligence.ppmtool.security.SecurityConstants.TOKEN_PREFIX;
 
 @RestController
 @RequestMapping("/api/users")
