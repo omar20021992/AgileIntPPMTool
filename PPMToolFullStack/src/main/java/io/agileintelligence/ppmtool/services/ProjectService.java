d788a8d6cb11308240ab5c0ef129a875f4e6d35b
omar20021992 <omar20021992@gmail.com>
2022-12-05 19:08:57 -0800

    idk

--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
@@ -2,9 +2,11 @@ package io.agileintelligence.ppmtool.services;
 
 import io.agileintelligence.ppmtool.domain.Backlog;
 import io.agileintelligence.ppmtool.domain.Project;
+import io.agileintelligence.ppmtool.domain.User;
 import io.agileintelligence.ppmtool.exceptions.ProjectIdException;
 import io.agileintelligence.ppmtool.repositories.BacklogRepository;
 import io.agileintelligence.ppmtool.repositories.ProjectRepository;
+import io.agileintelligence.ppmtool.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,8 +19,16 @@ public class ProjectService {
     @Autowired
     private BacklogRepository backlogRepository;
 
-    public Project saveOrUpdateProject(Project project){
+    @Autowired
+    private UserRepository userRepository;
+
+
+
+    public Project saveOrUpdateProject(Project project, String username){
         try{
+            User user = userRepository.findByUsername(username);
+            project.setUser(user);
+            project.setProjectLeader(user.getUsername());
             project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
 
             if(project.getId()==null){
@@ -27,46 +37,30 @@ public class ProjectService {
                 backlog.setProject(project);
                 backlog.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
             }
-
             if(project.getId()!=null){
                 project.setBacklog(backlogRepository.findByProjectIdentifier(project.getProjectIdentifier().toUpperCase()));
             }
-
             return projectRepository.save(project);
-
         }catch (Exception e){
             throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
         }
-
     }
-
-
     public Project findProjectByIdentifier(String projectId){
-
+        //Only want to return the project if the user looking for it is the owner
         Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
-
         if(project == null){
             throw new ProjectIdException("Project ID '"+projectId+"' does not exist");
-
         }
-
-
         return project;
     }
-
     public Iterable<Project> findAllProjects(){
         return projectRepository.findAll();
     }
-
-
     public void deleteProjectByIdentifier(String projectid){
         Project project = projectRepository.findByProjectIdentifier(projectid.toUpperCase());
-
         if(project == null){
             throw  new  ProjectIdException("Cannot Project with ID '"+projectid+"'. This project does not exist");
         }
-
         projectRepository.delete(project);
     }
-
 }
