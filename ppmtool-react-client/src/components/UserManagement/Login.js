5ac83f9ae66b2078fadefb24cc299042134959cc
omar20021992 <omar20021992@gmail.com>
2022-12-14 18:57:15 -0800

    login complited. doesnt go back to dashboard

--- ppmtool-react-client/src/components/UserManagement/Login.js
+++ ppmtool-react-client/src/components/UserManagement/Login.js
@@ -1,20 +1,57 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import classnames from "classnames";
+import { login } from "../../actions/securityActions";
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      password: ""
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.security.validToken) {
+      this.props.history.push("/dashboard");
+    }
+  
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const LoginRequest = {
+      username: this.state.username,
+      password: this.state.password
+    };
+
+    this.props.login(LoginRequest);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
   render() {
     return (
-        <div className="login">
+      <div className="login">
         <div className="container">
           <div className="row">
             <div className="col-md-8 m-auto">
               <h1 className="display-4 text-center">Log In</h1>
-              <form action="dashboard.html">
+              <form onSubmit={this.onSubmit}>
                 <div className="form-group">
                   <input
-                    type="email"
+                  type="text"
                     className="form-control form-control-lg"
                     placeholder="Email Address"
-                    name="email"
+                    name="username"
+                    value={this.state.username}
+                    onChange={this.onChange}
                   />
                 </div>
                 <div className="form-group">
@@ -23,6 +60,8 @@ class Login extends Component {
                     className="form-control form-control-lg"
                     placeholder="Password"
                     name="password"
+                    value={this.state.password}
+                    onChange={this.onChange}
                   />
                 </div>
                 <input type="submit" className="btn btn-info btn-block mt-4" />
@@ -31,8 +70,20 @@ class Login extends Component {
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+  errors: PropTypes.object.isRequired
+};
 
-export default Login;
+const mapStateToProps = state => ({
+  security: state.security,
+  errors: state.errors
+});
+
+export default connect(
+  mapStateToProps,
+  { login }
+)(Login);
